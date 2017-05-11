cd23c739339aa7b1cc8227ca21100c48fa8265b4
Andrew Gulik <andrewgulik@gmail.com>
2017-05-11 11:44:46 -0400

    finished modal fields

--- src/components/modals/CreateBoard.js
+++ src/components/modals/CreateBoard.js
@@ -7,12 +7,39 @@ export default class CreateBoard extends Component {
     this.state = {};
   }
 
+  _handleCreateBoard = () => {
+    let { title: {value: title}, description: {value: description} } = this.refs;
+
+    if (title && descriptions) {
+      auth.signup(email, password)
+      .then(res => this.props.router.push('/boards/:id'))
+      .catch(console.error)
+    }
+    else {
+      this.setState({ error: "Please enter both a title and description"})
+    }
+  }
+
+  _handleTyping = (e) => {
+    if (this.state && this.state.error) {
+      this.setState({ error: null })
+    }
+    if (e.keyCode===ENTER) {
+      this._handleSignUp()
+    }
+  }
+
   render() {
     return (
-      <div className='modal'>
+      <div className='create_board'>
         <h1>Create New Board</h1>
-        <p>Title</p>
-        <p>Description</p>
+        <input type="text" ref="title" placeholder="Title"
+          onKeyUp={this._handleTyping}
+        />
+        <input type="text" ref="description" placeholder="Description"
+          onKeyUp={this._handleTyping}
+        />
+        <button onClick={this._handleCreateBoard}>Create</button>
       </div>
     );
   }
